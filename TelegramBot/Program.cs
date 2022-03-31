1231ab7d8d9e06d2aa5ce856d648df4dab46511d
Oleksii Medvedenko <leshamedvedenko@gmail.com>
2022-03-31 15:10:38 +0200

    NewMenu

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -6,6 +6,7 @@ using System.Net;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBot
@@ -21,7 +22,7 @@ namespace TelegramBot
         static void Main(string[] args)
         {
             client = new TelegramBotClient(Token);
-            client.StartReceiving();
+            client.StartReceiving(new UpdateType[] { UpdateType.Message });
             client.OnMessage += OnMessageHandler;
             Console.ReadLine();
             client.StopReceiving();
@@ -34,65 +35,56 @@ namespace TelegramBot
 
             var choice = await client.SendTextMessageAsync(message.Chat.Id, "Оберіть своє місто зі списку або введіть його власноруч",
                         replyToMessageId: message.MessageId, replyMarkup: ChoiceCity());
-
+            
             var city = message;
 
-            while(message.Text != "Повернутись до вибору міста")
+            
+
+            switch (message.Text)
             {
-                switch (message.Text)
-                {
-                    case "Прогноз погоди на дану годину":
-                        weatherResponse = OneDayWeatherController.GetWeatherFromWebSite("Wroclaw");
-                        var Wroclaw = await client.SendTextMessageAsync(message.Chat.Id, "Прогноз погоди на дану годину",
-                             replyMarkup: ChoiceForecastForOneOrSevenDays());
-                        return;
+                case "Прогноз погоди на дану годину":
 
-                    case "Kropyvnytskyi":
-                        weatherResponse = OneDayWeatherController.GetWeatherFromWebSite("Kropyvnytskyi");
-                        var Kropyvnytskyi = await client.SendTextMessageAsync(message.Chat.Id, OneDayWeatherController.GetWeatherInfo(weatherResponse),
-                            replyToMessageId: message.MessageId, replyMarkup: GetOneDayButtons());
-                        break;
+                    var now = await client.SendTextMessageAsync(message.Chat.Id, "Прогноз погоди на дану годину",
+                            replyMarkup: ChoiceForecastForOneOrSevenDays());
+                    return;
 
-                    case "Послать русский корабль на х*й":
-                        var str = await client.SendTextMessageAsync(message.Chat.Id, "Ruskiy vojenyj korabl - idi naxyj!", replyMarkup: GetOneDayButtons());
+                case "Прогноз погоди на сьогодні":
+                    var now1 = await client.SendTextMessageAsync(message.Chat.Id, "Прогноз погоди на сьогодні",
+                         replyMarkup: ChoiceForecastForOneOrSevenDays());
+                    break;
 
-                        var pic = await client.SendPhotoAsync(chatId: message.Chat.Id,
-                            photo: "https://i1.sndcdn.com/artworks-1nniBHIeZ6z5fYnS-AzbPvg-t500x500.jpg",
-                            replyMarkup: GetOneDayButtons());
-                        break;
+                case "Прогноз погоди на завтра":
 
-                    default:
-                        weatherResponse = OneDayWeatherController.GetWeatherFromWebSite(message.Text);
+                    break;
 
-                        if (weatherResponse != null)
-                        {
-                            var anyCity = await client.SendTextMessageAsync(message.Chat.Id, OneDayWeatherController.GetWeatherInfo(weatherResponse),
-                            replyToMessageId: message.MessageId, replyMarkup: GetOneDayButtons());
+                case "Прогноз погоди на сім днів":
 
-                            break;
-                        }
-                        else
-                        {
-                            await client.SendTextMessageAsync(message.Chat.Id, "Перевірте чи поправно введена назва міста!", replyMarkup: GetOneDayButtons());
-                        }
+                    break;
 
-                        await client.SendTextMessageAsync(message.Chat.Id, "Оберіть ваше місто, або напишіть його самостійно на будь якій мові",
-                            replyMarkup: GetOneDayButtons());
-                        break;
+                case "Повернутись до вибору міста":
+                    var choice1 = await client.SendTextMessageAsync(message.Chat.Id, "Оберіть своє місто зі списку або введіть його власноруч",
+                        replyToMessageId: message.MessageId, replyMarkup: ChoiceCity());
+                    break;
 
-                        #region Ненужные команды
-                        //var sticker = await client.SendStickerAsync(
-                        //    chatId: message.Chat.Id,
-                        //    sticker: "https://cdn.tlgrm.app/stickers/96b/f1e/96bf1eca-a75d-3b7c-b620-bb5f2cdac89f/192/1.webp",
-                        //    replyToMessageId: message.MessageId,
-                        //    replyMarkup: GetButtons());
+                default:
 
-                        //var pic = await client.SendPhotoAsync(chatId: message.Chat.Id,
-                        //    photo: "https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/276262322_5303012999730593_2356718401733600899_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=zaCkZdsGqKUAX-FaZr0&tn=HZR5UL9zvrEjBtMF&_nc_ht=scontent-vie1-1.xx&oh=00_AT_Bz9bhTfCQTOENnax_o-QBrmC5wMLwHs_8_GtgKVwT2g&oe=6248AA28",
-                        //    replyMarkup: GetButtons());
-                        #endregion
-                }
+                    await client.SendTextMessageAsync(message.Chat.Id, "...",
+                        replyMarkup: ChoiceCity());
+                    break;
+
+                    #region Ненужные команды
+                    //var sticker = await client.SendStickerAsync(
+                    //    chatId: message.Chat.Id,
+                    //    sticker: "https://cdn.tlgrm.app/stickers/96b/f1e/96bf1eca-a75d-3b7c-b620-bb5f2cdac89f/192/1.webp",
+                    //    replyToMessageId: message.MessageId,
+                    //    replyMarkup: GetButtons());
+
+                    //var pic = await client.SendPhotoAsync(chatId: message.Chat.Id,
+                    //    photo: "https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/276262322_5303012999730593_2356718401733600899_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=zaCkZdsGqKUAX-FaZr0&tn=HZR5UL9zvrEjBtMF&_nc_ht=scontent-vie1-1.xx&oh=00_AT_Bz9bhTfCQTOENnax_o-QBrmC5wMLwHs_8_GtgKVwT2g&oe=6248AA28",
+                    //    replyMarkup: GetButtons());
+                    #endregion
             }
+
         }
 
         private static IReplyMarkup ChoiceCity()
@@ -102,18 +94,6 @@ namespace TelegramBot
                 Keyboard = new List<List<KeyboardButton>>
                 {
                     new List<KeyboardButton> { new KeyboardButton { Text = $"Wroclaw" },  new KeyboardButton { Text = "Kropyvnytskyi" } },
-                }
-            };
-        }
-
-        private static IReplyMarkup GetOneDayButtons()
-        {
-            return new ReplyKeyboardMarkup
-            {
-                Keyboard = new List<List<KeyboardButton>>
-                {
-                    new List<KeyboardButton> { new KeyboardButton { Text = $"Wroclaw" },  new KeyboardButton { Text = "Kropyvnytskyi" } },
-                    new List<KeyboardButton> { new KeyboardButton { Text = "Послать русский корабль на х*й" } }
                 }
             };
         }
@@ -131,65 +111,65 @@ namespace TelegramBot
             };
         }
 
-        private static async void GetLogicForOneDayForecast(Message message)
-        {
-            var choice = await client.SendTextMessageAsync(message.Chat.Id, "Оберіть",
-                         replyMarkup: GetOneDayButtons());
-            Console.ReadLine();
+        //private static async void GetLogicForOneDayForecast(Message message)
+        //{
+        //    var choice = await client.SendTextMessageAsync(message.Chat.Id, "Оберіть",
+        //                 replyMarkup: GetOneDayButtons());
+        //    Console.ReadLine();
 
-            switch (message.Text)
-            {
-                case "Wroclaw":
-                    weatherResponse = OneDayWeatherController.GetWeatherFromWebSite("Wroclaw");
-                    var Wroclaw = await client.SendTextMessageAsync(message.Chat.Id, OneDayWeatherController.GetWeatherInfo(weatherResponse),
-                        replyToMessageId: message.MessageId, replyMarkup: GetOneDayButtons());
-                    return;
+        //    switch (message.Text)
+        //    {
+        //        case "Wroclaw":
+        //            weatherResponse = OneDayWeatherController.GetWeatherFromWebSite("Wroclaw");
+        //            var Wroclaw = await client.SendTextMessageAsync(message.Chat.Id, OneDayWeatherController.GetWeatherInfo(weatherResponse),
+        //                replyToMessageId: message.MessageId, replyMarkup: GetOneDayButtons());
+        //            return;
 
-                case "Kropyvnytskyi":
-                    weatherResponse = OneDayWeatherController.GetWeatherFromWebSite("Kropyvnytskyi");
-                    var Kropyvnytskyi = await client.SendTextMessageAsync(message.Chat.Id, OneDayWeatherController.GetWeatherInfo(weatherResponse),
-                        replyToMessageId: message.MessageId, replyMarkup: GetOneDayButtons());
-                    break;
+        //        case "Kropyvnytskyi":
+        //            weatherResponse = OneDayWeatherController.GetWeatherFromWebSite("Kropyvnytskyi");
+        //            var Kropyvnytskyi = await client.SendTextMessageAsync(message.Chat.Id, OneDayWeatherController.GetWeatherInfo(weatherResponse),
+        //                replyToMessageId: message.MessageId, replyMarkup: GetOneDayButtons());
+        //            break;
 
-                case "Послать русский корабль на х*й":
-                    var str = await client.SendTextMessageAsync(message.Chat.Id, "Ruskiy vojenyj korabl - idi naxyj!", replyMarkup: GetOneDayButtons());
+        //        case "Послать русский корабль на х*й":
+        //            var str = await client.SendTextMessageAsync(message.Chat.Id, "Ruskiy vojenyj korabl - idi naxyj!", replyMarkup: GetOneDayButtons());
 
-                    var pic = await client.SendPhotoAsync(chatId: message.Chat.Id,
-                        photo: "https://i1.sndcdn.com/artworks-1nniBHIeZ6z5fYnS-AzbPvg-t500x500.jpg",
-                        replyMarkup: GetOneDayButtons());
-                    break;
+        //            var pic = await client.SendPhotoAsync(chatId: message.Chat.Id,
+        //                photo: "https://i1.sndcdn.com/artworks-1nniBHIeZ6z5fYnS-AzbPvg-t500x500.jpg",
+        //                replyMarkup: GetOneDayButtons());
+        //            break;
 
-                default:
-                    weatherResponse = OneDayWeatherController.GetWeatherFromWebSite(message.Text);
+        //        default:
+        //            weatherResponse = OneDayWeatherController.GetWeatherFromWebSite(message.Text);
 
-                    if (weatherResponse != null)
-                    {
-                        var anyCity = await client.SendTextMessageAsync(message.Chat.Id, OneDayWeatherController.GetWeatherInfo(weatherResponse),
-                        replyToMessageId: message.MessageId, replyMarkup: GetOneDayButtons());
+        //            if (weatherResponse != null)
+        //            {
+        //                var anyCity = await client.SendTextMessageAsync(message.Chat.Id, OneDayWeatherController.GetWeatherInfo(weatherResponse),
+        //                replyToMessageId: message.MessageId, replyMarkup: GetOneDayButtons());
 
-                        break;
-                    }
-                    else
-                    {
-                        await client.SendTextMessageAsync(message.Chat.Id, "Перевірте чи поправно введена назва міста!", replyMarkup: GetOneDayButtons());
-                    }
+        //                break;
+        //            }
+        //            else
+        //            {
+        //                await client.SendTextMessageAsync(message.Chat.Id, "Перевірте чи поправно введена назва міста!", replyMarkup: GetOneDayButtons());
+        //            }
 
-                    await client.SendTextMessageAsync(message.Chat.Id, "Оберіть ваше місто, або напишіть його самостійно на будь якій мові",
-                        replyMarkup: GetOneDayButtons());
-                    break;
+        //            await client.SendTextMessageAsync(message.Chat.Id, "Оберіть ваше місто, або напишіть його самостійно на будь якій мові",
+        //                replyMarkup: GetOneDayButtons());
+        //            break;
 
-                    #region Ненужные команды
-                    //var sticker = await client.SendStickerAsync(
-                    //    chatId: message.Chat.Id,
-                    //    sticker: "https://cdn.tlgrm.app/stickers/96b/f1e/96bf1eca-a75d-3b7c-b620-bb5f2cdac89f/192/1.webp",
-                    //    replyToMessageId: message.MessageId,
-                    //    replyMarkup: GetButtons());
+        //            #region Ненужные команды
+        //            //var sticker = await client.SendStickerAsync(
+        //            //    chatId: message.Chat.Id,
+        //            //    sticker: "https://cdn.tlgrm.app/stickers/96b/f1e/96bf1eca-a75d-3b7c-b620-bb5f2cdac89f/192/1.webp",
+        //            //    replyToMessageId: message.MessageId,
+        //            //    replyMarkup: GetButtons());
 
-                    //var pic = await client.SendPhotoAsync(chatId: message.Chat.Id,
-                    //    photo: "https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/276262322_5303012999730593_2356718401733600899_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=zaCkZdsGqKUAX-FaZr0&tn=HZR5UL9zvrEjBtMF&_nc_ht=scontent-vie1-1.xx&oh=00_AT_Bz9bhTfCQTOENnax_o-QBrmC5wMLwHs_8_GtgKVwT2g&oe=6248AA28",
-                    //    replyMarkup: GetButtons());
-                    #endregion
-            }
-        }
+        //            //var pic = await client.SendPhotoAsync(chatId: message.Chat.Id,
+        //            //    photo: "https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/276262322_5303012999730593_2356718401733600899_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=zaCkZdsGqKUAX-FaZr0&tn=HZR5UL9zvrEjBtMF&_nc_ht=scontent-vie1-1.xx&oh=00_AT_Bz9bhTfCQTOENnax_o-QBrmC5wMLwHs_8_GtgKVwT2g&oe=6248AA28",
+        //            //    replyMarkup: GetButtons());
+        //            #endregion
+        //    }
+        //}
     }
 }
